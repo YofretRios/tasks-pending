c309d143e8173a6c4eb9604eeca1075bc6e34721
Yofret Rios <yofret.rios@firstleaf.com>
2021-12-07 20:13:53 -0500

    feat: adding more comments

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,14 @@
 import dynamic from 'next/dynamic';
-
-// Get Header without SSR
+// Prevent Header from rendering on the server to avoid missmatch HTML
 const Header = dynamic(() => import('./Header'), { ssr: false });
-
-// import Header from "./Header";
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <main className="max-w-7xl mx-auto px-2 py-10 sm:px-6 lg:px-8">{children}</main>
+      <main className="max-w-7xl mx-auto px-2 py-10 sm:px-6 lg:px-8">
+        {children}
+      </main>
     </>
   );
 }
